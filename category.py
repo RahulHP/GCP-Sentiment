79deee3ef5fef25e1522e2352b088c359935e29e
RahulHP <rahul.hphatak@gmail.com>
2018-08-07 03:58:34 +0000

    Changing category files 1/2

--- category.py
+++ category.py
@@ -3,13 +3,13 @@ class Category:
 		super(Category, self).__init__()
 		self.name = name
 		self.download_url = 'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_'+name+'_5.json.gz'
-		self.reviews_downloaded_json = 'gs://pysenti-data/reviews/downloaded/'+name+'.json'
+		self.reviews_downloaded_json = 'gs://pysenti-data/reviews/downloaded/reviews_'+name+'_5.json'
 		self.reviews_processed_parquet = 'gs://pysenti-data/reviews/processed/'+name+'.parquet'
 		self.reviews_train_parquet = 'gs://pysenti-data/reviews/train/'+name+'.parquet'
 		self.reviews_test_parquet = 'gs://pysenti-data/reviews/test/'+name+'.parquet'
 
-		self.lexicons_raw_folder = 'gs://lexicons/raw/'+name
-		self.lexicons_csv = 'gs://lexicons/processed/'+name+'.csv'
+		self.lexicons_raw_folder = 'gs://pysenti-data/lexicons/raw/'+name
+		self.lexicons_csv = 'gs://pysenti-data/lexicons/processed/'+name+'.csv'
 
 	def __str__(self):
 		return self.name
--- categorygenerator.py
+++ categorygenerator.py
@@ -1,11 +1,10 @@
-from google.cloud import storage
+from gcloud import storage
 from category import Category
-from google.cloud import storage
 
 def CategoryGenerator(bucket,file_path):
 	client = storage.Client()
 	bucket = client.get_bucket(bucket)
 	src_url_file = bucket.get_blob(file_path)
 	src_url_string = src_url_file.download_as_string()
-	for category_name in self.src_url_string.split('\r\n'):
+	for category_name in src_url_string.decode('utf-8').split('\r\n'):
 		yield Category(category_name)
